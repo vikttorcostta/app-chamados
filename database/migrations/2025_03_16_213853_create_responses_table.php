f407d094e907e5e6fbe679bd335d920a44b993b2
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-16 18:42:54 -0300

    criação de models e migrations

--- database/migrations/2025_03_16_213853_create_responses_table.php
+++ database/migrations/2025_03_16_213853_create_responses_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('responses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'user', 'support'])->default('user');
-            $table->rememberToken();
+            $table->text('message');
+            $table->dateTime('date');
+
+            $table->foreignId('ticket_id')->constrained('tickets');
+            $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('responses');
     }
 };
