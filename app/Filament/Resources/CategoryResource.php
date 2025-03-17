f57c294085520f13a71c374277966db53414fd88
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-17 13:38:46 -0300

    botões de ações view, edit, delete em CategoryResource

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -54,7 +54,9 @@ class CategoryResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
