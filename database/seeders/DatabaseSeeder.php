ac9ed9d18474856515e60338fc4565baad52b7e2
Paulo Victor Costa <victorcosta.bsi@gmail.com>
2025-03-16 23:08:35 -0300

    criação de CategoryFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,68 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+//         \App\Models\User::factory()->create([
+//             'name' => 'Victor Costa',
+//             'email' => 'victorcosta@bugados.com.br',
+//             'password' => bcrypt('12345678'),
+//             'role' => 'user',
+//         ]);
+//
+//        \App\Models\User::factory()->create([
+//            'name' => 'Davi Caridade',
+//            'email' => 'davicaridade@bugados.com.br',
+//            'password' => bcrypt('12345678'),
+//            'role' => 'user',
+//        ]);
+//
+//        \App\Models\User::factory()->create([
+//            'name' => 'Gabriel Gomes',
+//            'email' => 'gabrielgomes@bugados.com.br',
+//            'password' => bcrypt('12345678'),
+//            'role' => 'support',
+//        ]);
+//
+//        \App\Models\User::factory()->create([
+//            'name' => 'Vandson Nascimento',
+//            'email' => 'vandson@bugados.com.br',
+//            'password' => bcrypt('12345678'),
+//            'role' => 'admin',
+//        ]);
+
+//        \App\Models\Cate::factory()->create([
+//            'name' => 'Problemas com Computador'
+//        ]);
+//
+//        \App\Models\Category::factory()->create([
+//            'name' => 'Internet e Rede '
+//        ]);
+//
+//        \App\Models\Category::factory()->create([
+//            'name' => 'Softwares e Sistemas'
+//        ]);
+//
+        \App\Models\Category::factory()->create([
+            'name' => 'Impressoras e Periféricos'
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Telefonia e Comunicação'
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Segurança e Ameaças'
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Backup e Arquivos'
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Equipamentos Novos'
+        ]);
+
+        \App\Models\Category::factory()->create([
+            'name' => 'Outros'
+        ]);
     }
 }
